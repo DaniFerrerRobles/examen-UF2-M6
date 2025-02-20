20ee6057d43b43eab1a48e93209e06271340013c
DaniFerrerRobles <120500268+DaniFerrerRobles@users.noreply.github.com>
2025-02-20 21:50:31 +0000

    .

--- src/components/ComeCocos.jsx
+++ src/components/ComeCocos.jsx
@@ -7,9 +7,8 @@ export const ComeCocos = ({ iconoCC }) => {
       {iconoCC.map((fila, idFila) => (
         <div key={idFila} className="divFilaCC">
           {fila.map((casilla, idCasilla) => (
-            <div
-              key={idCasilla}
-              className={`divCasillaCC ${casilla === 1 ? "comecocos" : ""}`}
+            <div key={idCasilla}
+              className={`divCasillaCC ${casilla === 1 ? "coincide" : "noCoincide"}`}
             ></div>
           ))}
         </div>
