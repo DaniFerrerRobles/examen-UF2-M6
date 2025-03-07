27c6be7d0dd2422980ca856341ceeb7b0a6ecfa3
DaniFerrerRobles <120500268+DaniFerrerRobles@users.noreply.github.com>
2025-03-07 23:46:31 +0000

    .

--- src/views/Juego.jsx
+++ src/views/Juego.jsx
@@ -1,18 +1,25 @@
-import React, { useState } from "react"
+import React, { useState } from "react";
 import { modelo } from "../components/cuadricula";
 import Cuadricula from "../components/Cuadricula";
 import ComeCocos from "../components/ComeCocos";
 
 const Juego = () => {
+  const filas = modelo.matrizCuadricula.length;
+  const columnas = modelo.matrizCuadricula[0].length;
+
+  const filaCentro = Math.floor(filas / 2);
+  const columnaCentro = Math.floor(columnas / 2);
+
   const [cuadriculaCC, setCuadriculasCC] = useState(modelo.matrizCuadricula);
-  const [posicionIconoCC, setIPosicionIonoCC] = useState(modelo.matrizCuadricula[10][10]);
+  const [posicionIconoCC, setPosicionIconoCC] = useState([filaCentro, columnaCentro]);
   const [iconoCC, setIconoCC] = useState(modelo.matrizComecocos);
 
   return (
-<div>
-      <Cuadricula cuadriculaCC = {cuadriculaCC} />
-      <ComeCocos iconoCC = {iconoCC} />
-</div>
-  )
-} //creamos el componente Juego que renderiza los componentes Cuadricula y ComeCocos
-export default Juego
+    <div>
+      <Cuadricula cuadriculaCC={cuadriculaCC} />
+      <ComeCocos posicionCC={posicionIconoCC} iconoCC={iconoCC} />
+    </div>
+  );
+};
+
+export default Juego;
